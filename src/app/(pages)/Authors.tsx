1858525e1380b3b7e10ba01c7269092fde39cabc
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2024-01-12 03:30:22 +0400

    authors page responsive

--- src/app/(pages)/Authors.tsx
+++ src/app/(pages)/Authors.tsx
@@ -1,11 +1,102 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 
+import styles from '../assets/style/Authors.module.css';
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 type Props = {}
 
 const Authors = (props: Props) => {
   return (
-    <Navbar/>
+    <>
+      <Navbar />
+      <div className={styles.authorsContainer}>
+        <div className={styles.authorOperations}>
+          <input className={styles.authorOperationsInput} placeholder='Search Author' type="text" />
+          <select className={styles.authorOperationsSelect}>
+            <option value="">Gender</option>
+            <option value="Male">Male</option>
+            <option value="Male">Female</option>
+          </select>
+        </div>
+        
+        <div className={styles.authorCards}>
+          <div className="row">
+            <div className="col-12 col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6">
+              <div className={`card ${styles.authorCard}`} > 
+                <img className={`card-img-top ${styles.authorCardImage}`} src={"https://hips.hearstapps.com/hmg-prod/images/gettyimages-1061157246.jpg"} alt="Card image cap"/>
+                  <div className="card-body">
+                    <h5 className="card-title">Joanne Rowling</h5>
+                    <p className="card-text">59 years old</p>
+                    <p className="card-text">Genre: Fantasy</p>
+                    <p className="card-text">Gender: Female</p>
+                    <div className='d-flex justify-content-center '>
+                      <button className='btn btn-danger'>DELETE</button>
+                    </div>
+                  </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6">
+              <div className={`card ${styles.authorCard}`} > 
+                <img className={`card-img-top ${styles.authorCardImage}`} src={"https://hips.hearstapps.com/hmg-prod/images/gettyimages-1061157246.jpg"} alt="Card image cap"/>
+                  <div className="card-body">
+                    <h5 className="card-title">Joanne Rowling</h5>
+                    <p className="card-text">59 years old</p>
+                    <p className="card-text">Genre: Fantasy</p>
+                    <p className="card-text">Gender: Female</p>
+                    <div className='d-flex justify-content-center '>
+                      <button className='btn btn-danger'>DELETE</button>
+                    </div>
+                  </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6">
+              <div className={`card ${styles.authorCard}`} > 
+                <img className={`card-img-top ${styles.authorCardImage}`} src={"https://hips.hearstapps.com/hmg-prod/images/gettyimages-1061157246.jpg"} alt="Card image cap"/>
+                  <div className="card-body">
+                    <h5 className="card-title">Joanne Rowling</h5>
+                    <p className="card-text">59 years old</p>
+                    <p className="card-text">Genre: Fantasy</p>
+                    <p className="card-text">Gender: Female</p>
+                    <div className='d-flex justify-content-center '>
+                      <button className='btn btn-danger'>DELETE</button>
+                    </div>
+                  </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6">
+              <div className={`card ${styles.authorCard}`} > 
+                <img className={`card-img-top ${styles.authorCardImage}`} src={"https://hips.hearstapps.com/hmg-prod/images/gettyimages-1061157246.jpg"} alt="Card image cap"/>
+                  <div className="card-body">
+                    <h5 className="card-title">Joanne Rowling</h5>
+                    <p className="card-text">59 years old</p>
+                    <p className="card-text">Genre: Fantasy</p>
+                    <p className="card-text">Gender: Female</p>
+                    <div className='d-flex justify-content-center '>
+                      <button className='btn btn-danger'>DELETE</button>
+                    </div>
+                  </div>
+              </div>
+            </div>
+
+           
+
+            
+
+            
+
+            
+
+            
+          </div>
+        </div>
+      </div>
+    </>
+
+
   )
 }
 
