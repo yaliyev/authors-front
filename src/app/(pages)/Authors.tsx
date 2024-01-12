3affe4450e5746fd17a713d14dc5756ffae468f3
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2024-01-12 07:22:43 +0400

    dynamic cards

--- src/app/(pages)/Authors.tsx
+++ src/app/(pages)/Authors.tsx
@@ -1,13 +1,28 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 
 import styles from '../assets/style/Authors.module.css';
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { useRouter } from 'next/navigation'
+import { getAuthors } from '../services/api/author_request';
 type Props = {}
 
 const Authors = (props: Props) => {
+
+ 
+  const [authorCards,setAuthorCards] =  useState<any>([]);
+
+  useEffect(()=>{
+      async function loadAuthors(){
+        const data = await getAuthors();
+
+        setAuthorCards(data);
+      }
+
+      loadAuthors();
+  },[]);
+
   const router = useRouter();
   return (
     <>
@@ -21,69 +36,30 @@ const Authors = (props: Props) => {
             <option value="female">Female</option>
           </select>
         </div>
+
+        
         
         <div className={styles.authorCards}>
           <div className="row">
-            <div onClick={()=>{router.push('/authors/2')}} className="col-12 col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6">
-              <div className={`card ${styles.authorCard}`} > 
-                <img className={`card-img-top ${styles.authorCardImage}`} src={"https://hips.hearstapps.com/hmg-prod/images/gettyimages-1061157246.jpg"} alt="Card image cap"/>
-                  <div className="card-body">
-                    <h5 className="card-title">Joanne Rowling</h5>
-                    <p className="card-text">59 years old</p>
-                    <p className="card-text">Genre: Fantasy</p>
-                    <p className="card-text">Gender: Female</p>
-                    <div className='d-flex justify-content-center '>
-                      <button className='btn btn-danger'>DELETE</button>
-                    </div>
+          {authorCards&&authorCards.map((card:any,index:any)=>{
+            return <div key={index} onClick={()=>{router.push(`/authors/${card.id}`)}} className="col-12 col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6">
+            <div className={`card ${styles.authorCard}`} > 
+              <img className={`card-img-top ${styles.authorCardImage}`} src={card.authorImage} alt="Card image cap"/>
+                <div className="card-body">
+                  <h5 className="card-title">{card.name}</h5>
+                  <p className="card-text">{new Date().getFullYear() - card.birthYear} years old</p>
+                  <p className="card-text">Genre: {card.genre}</p>
+                  <p className="card-text">Gender: {card.gender}</p>
+                  <div className='d-flex justify-content-center '>
+                    <button className='btn btn-danger'>DELETE</button>
                   </div>
-              </div>
+                </div>
             </div>
+          </div>;
+          })}
+            
 
-            <div className="col-12 col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6">
-              <div className={`card ${styles.authorCard}`} > 
-                <img className={`card-img-top ${styles.authorCardImage}`} src={"https://hips.hearstapps.com/hmg-prod/images/gettyimages-1061157246.jpg"} alt="Card image cap"/>
-                  <div className="card-body">
-                    <h5 className="card-title">Joanne Rowling</h5>
-                    <p className="card-text">59 years old</p>
-                    <p className="card-text">Genre: Fantasy</p>
-                    <p className="card-text">Gender: Female</p>
-                    <div className='d-flex justify-content-center '>
-                      <button className='btn btn-danger'>DELETE</button>
-                    </div>
-                  </div>
-              </div>
-            </div>
-
-            <div className="col-12 col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6">
-              <div className={`card ${styles.authorCard}`} > 
-                <img className={`card-img-top ${styles.authorCardImage}`} src={"https://hips.hearstapps.com/hmg-prod/images/gettyimages-1061157246.jpg"} alt="Card image cap"/>
-                  <div className="card-body">
-                    <h5 className="card-title">Joanne Rowling</h5>
-                    <p className="card-text">59 years old</p>
-                    <p className="card-text">Genre: Fantasy</p>
-                    <p className="card-text">Gender: Female</p>
-                    <div className='d-flex justify-content-center '>
-                      <button className='btn btn-danger'>DELETE</button>
-                    </div>
-                  </div>
-              </div>
-            </div>
-
-            <div className="col-12 col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6">
-              <div className={`card ${styles.authorCard}`} > 
-                <img className={`card-img-top ${styles.authorCardImage}`} src={"https://hips.hearstapps.com/hmg-prod/images/gettyimages-1061157246.jpg"} alt="Card image cap"/>
-                  <div className="card-body">
-                    <h5 className="card-title">Joanne Rowling</h5>
-                    <p className="card-text">59 years old</p>
-                    <p className="card-text">Genre: Fantasy</p>
-                    <p className="card-text">Gender: Female</p>
-                    <div className='d-flex justify-content-center '>
-                      <button className='btn btn-danger'>DELETE</button>
-                    </div>
-                  </div>
-              </div>
-            </div>
-
+           
            
 
             
--- src/app/services/api/author_request.ts
+++ src/app/services/api/author_request.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+export async function getAuthors(){
+    return await axios.get('http://localhost:5100/').then(response=>response.data);
+}
 
 export async function postAuthor(author:any){
-    return axios.post('http://localhost:5100/',author);
+    return await axios.post('http://localhost:5100/',author);
 }
