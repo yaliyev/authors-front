cf07ade31820c8d67dfd70687de10e6e8cfc3bfb
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2024-01-15 04:04:07 +0400

    downloading files

--- src/app/(pages)/authors/[authorId]/page.tsx
+++ src/app/(pages)/authors/[authorId]/page.tsx
@@ -6,6 +6,7 @@ import styles from '../../../assets/style/AuthorDetail.module.css';
 import "../../../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { getAuthorById } from '@/app/services/api/author_request';
 import { useRouter } from 'next/navigation';
+import { getBooks } from '@/app/services/api/book_request';
 type Props = {}
 
 const AuthorDetail = (props: { params: any }) => {
@@ -14,6 +15,7 @@ const AuthorDetail = (props: { params: any }) => {
 
 
   const [author, setAuthor] = useState<Author>();
+  const [books, setBooks] = useState<Book[]>();
 
   useEffect(() => {
     async function loadAuthor() {
@@ -22,8 +24,14 @@ const AuthorDetail = (props: { params: any }) => {
 
       setAuthor(author);
     }
+    async function loadBooks() {
+      let booksData = await getBooks();
+
+      setBooks(booksData);
+    }
 
     loadAuthor();
+    loadBooks();
 
   }, [])
 
@@ -41,8 +49,8 @@ const AuthorDetail = (props: { params: any }) => {
             </div>
             <div className="col-12 col-xxl-6 col-xl-6 col-lg-6 col-md-12">
               <div className={styles.authorDetailButtons}>
-                <button onClick={()=>{router.push(`/authors/${author._id}/editAuthor`)}} className={`btn btn-warning text-white ${styles.authorDetailButton}`}>Edit Author</button>
-                <button onClick={()=>{router.push(`/authors/${author._id}/addBook`)}} className={`btn btn-success text-white ${styles.authorDetailButton}`}>Add book</button>
+                <button onClick={() => { router.push(`/authors/${author._id}/editAuthor`) }} className={`btn btn-warning text-white ${styles.authorDetailButton}`}>Edit Author</button>
+                <button onClick={() => { router.push(`/authors/${author._id}/addBook`) }} className={`btn btn-success text-white ${styles.authorDetailButton}`}>Add book</button>
               </div>
 
               <div className={styles.authorDetail}>
@@ -56,6 +64,25 @@ const AuthorDetail = (props: { params: any }) => {
               </div>
 
             </div>
+
+            {books && books.map((book: Book, index: number) => {
+              return <div key={index} className="col-12 mt-5 mb-4">
+                <div className="col-12 col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6">
+                  <div className={`card`} >
+                    <img className={`card-img-top`} src={book.coverImg}  />
+                    <div className="card-body">
+                      <h5 className="card-title">{book.name}</h5>
+                      <p className="card-text">Year: {book.year}</p>
+                      <p className="card-text">Description:  {book.description}</p>
+                      <div className='d-flex justify-content-center '>
+                        <a href={book.bookFile.slice(0,book.bookFile.indexOf('/upload')+7)+`/fl_attachment:${book.name}`+book.bookFile.slice(book.bookFile.indexOf('/upload')+7)} className='btn btn-primary'>Download</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>;
+            })}
+
           </> :
             <>
 
---
+++ src/app/types/book.d.ts
@@ -0,0 +1,9 @@
+type Book = {
+    _id: string,
+    name: string,
+    year: number,
+    coverImg: string,
+    description: string,
+    bookFile: string,
+    authorId: string,
+}
